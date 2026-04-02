13860a899938b78b6b824ac5f6674e743d82a0d6
ja007MJ <jai007nnn@gmail.com>
2026-04-02 16:29:32 +0530

    changes in file

--- resources/js/pages/Vendor/Partials/DeleteUserForm.vue
+++ resources/js/pages/Vendor/Partials/DeleteUserForm.vue
@@ -1,10 +1,10 @@
 <script setup>
-import DangerButton from '@/Components/DangerButton';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import Modal from '@/Components/Modal';
-import SecondaryButton from '@/Components/SecondaryButton';
-import TextInput from '@/Components/TextInput';
+import DangerButton from '@/Components/DangerButton.vue';
+import InputError from '@/Components/InputError.vue';
+import InputLabel from '@/Components/InputLabel.vue';
+import Modal from '@/Components/Modal.vue';
+import SecondaryButton from '@/Components/SecondaryButton.vue';
+import TextInput from '@/Components/TextInput.vue';
 
 </script>
 <template>
